c0afad961b0b35955049515e2588f0522a5cba46
QuanBiXeChoi <24521454@gm.uit.edu.vn>
2025-10-24 20:39:30 +0700

    Bai 6

---
+++ Bai03/Client.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Bai03
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtChatLog = new System.Windows.Forms.TextBox();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtChatLog
+            // 
+            this.txtChatLog.Location = new System.Drawing.Point(38, 79);
+            this.txtChatLog.Multiline = true;
+            this.txtChatLog.Name = "txtChatLog";
+            this.txtChatLog.ReadOnly = true;
+            this.txtChatLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtChatLog.Size = new System.Drawing.Size(620, 236);
+            this.txtChatLog.TabIndex = 0;
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.Location = new System.Drawing.Point(722, 79);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(166, 50);
+            this.btn_Connect.TabIndex = 1;
+            this.btn_Connect.Text = "Connect";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Location = new System.Drawing.Point(722, 367);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(166, 50);
+            this.btn_Send.TabIndex = 2;
+            this.btn_Send.Text = "Send";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(38, 377);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(620, 31);
+            this.txtMessage.TabIndex = 4;
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 481);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btn_Send);
+            this.Controls.Add(this.btn_Connect);
+            this.Controls.Add(this.txtChatLog);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Client_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtChatLog;
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.TextBox txtMessage;
+    }
+}
+
---
+++ Bai03/Client.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai03
+{
+    public partial class Client : Form
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private Thread receiveThread;
+
+        private const string Serverip = "127.0.0.1";
+        private const int port = 8000;
+        public Client()
+        {
+            InitializeComponent();
+            btn_Send.Enabled = false;
+        }
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new TcpClient();
+                client.Connect(Serverip, port);
+
+                stream = client.GetStream();
+
+                receiveThread = new Thread(new ThreadStart(ReceiveMessages));
+                receiveThread.IsBackground = true;
+                receiveThread.Start();
+
+                UpdateChatLog("Đã kết nối thành công đến Server!");
+                btn_Connect.Enabled = false;
+                btn_Send.Enabled = true;
+            }
+            catch (Exception ex) 
+            {
+                UpdateChatLog("Lỗi kết nối Server: " + ex.Message);
+                btn_Connect.Enabled = true;
+            }
+        }
+
+        private void btn_Send_Click(object sender, EventArgs e)
+        {
+            string Message = txtMessage.Text;
+            if (string.IsNullOrWhiteSpace(Message) || stream == null) return;
+
+            try
+            {
+                byte[] data = Encoding.ASCII.GetBytes(Message);
+
+                stream.Write(data, 0, data.Length);
+
+                UpdateChatLog($"[You]: {Message}");
+
+                txtMessage.Clear();
+            }
+            catch (Exception ex)
+            {
+                UpdateChatLog("Lỗi gửi tin nhắn: " + ex.Message);
+                Disconnect();
+            }
+        }
+
+        private void UpdateChatLog(string message)
+        {
+            if (this.txtChatLog.InvokeRequired)
+            {
+                this.Invoke(new Action<string>(UpdateChatLog), new object[] { message });
+            }
+            else
+            {
+                txtChatLog.AppendText(message + Environment.NewLine);
+            }
+        }
+
+        private void Disconnect()
+        {
+            if (client != null)
+            {
+                stream?.Close();
+                client.Close();
+                stream = null;
+                client = null;
+                UpdateChatLog("Đã ngắt kết nối.");
+
+                
+                this.Invoke(new Action(() => {
+                    btn_Connect.Enabled = true;
+                    btn_Send.Enabled = false;
+                }));
+            }
+            
+            if (receiveThread != null && receiveThread.IsAlive)
+            {
+                receiveThread.Abort();
+            }
+        }
+
+        private void ReceiveMessages()
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            try
+            {
+                while (client.Connected)
+                {
+                    bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                    if (bytesRead == 0)
+                    {
+                        UpdateChatLog("Server đã ngắt kết nối!");
+                        break;
+                    }
+
+                    string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    UpdateChatLog($"[Server]: {response}");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                UpdateChatLog("Lỗi nhận tin nhắn hoặc kết nối thất bại" + ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+        private void ClientForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Disconnect();
+        }
+
+        private void Client_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Disconnect();
+        }
+    }
+}
