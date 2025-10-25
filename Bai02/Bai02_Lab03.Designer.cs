631f0b32db506a0dbc74fa4447811c6f029cd30f
TheAnh-05 <nguyenquangtheanh2005@gmail.com>
2025-10-25 16:21:46 +0700

    Bai02

---
+++ Bai02/Bai02_Lab03.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Bai02
+{
+    partial class Bai02_Lab03
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Listen = new System.Windows.Forms.Button();
+            this.txt_Listen = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Listen
+            // 
+            this.btn_Listen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Listen.Location = new System.Drawing.Point(576, 61);
+            this.btn_Listen.Name = "btn_Listen";
+            this.btn_Listen.Size = new System.Drawing.Size(132, 45);
+            this.btn_Listen.TabIndex = 0;
+            this.btn_Listen.Text = "Listen";
+            this.btn_Listen.UseVisualStyleBackColor = true;
+            this.btn_Listen.Click += new System.EventHandler(this.btn_Listen_Click);
+            // 
+            // txt_Listen
+            // 
+            this.txt_Listen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Listen.Location = new System.Drawing.Point(108, 123);
+            this.txt_Listen.Multiline = true;
+            this.txt_Listen.Name = "txt_Listen";
+            this.txt_Listen.Size = new System.Drawing.Size(600, 274);
+            this.txt_Listen.TabIndex = 1;
+            // 
+            // Bai02_Lab03
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_Listen);
+            this.Controls.Add(this.btn_Listen);
+            this.Name = "Bai02_Lab03";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Listen;
+        private System.Windows.Forms.TextBox txt_Listen;
+    }
+}
+
---
+++ Bai02/Bai02_Lab03.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai02
+{
+    public partial class Bai02_Lab03 : Form
+    {
+        public Bai02_Lab03()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Listen_Click(object sender, EventArgs e)
+        {
+            void StartUnsafeThread()
+            {
+                int bytesReceived = 0;
+                byte[] arr = new byte[1];
+                Socket clientSocket;
+                Socket listenerSocket = new Socket(
+                    AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+                //Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                listenerSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                listenerSocket.Bind(new IPEndPoint(IPAddress.Any, 8080));
+                listenerSocket.Listen(-1);
+                clientSocket = listenerSocket.Accept();
+                txt_Listen.AppendText("New client connected" + Environment.NewLine);
+                while (clientSocket.Connected)
+                {
+                    try
+                    {
+                        string text = "";
+                        do
+                        {
+                            try
+                            {
+                                bytesReceived = clientSocket.Receive(arr);
+                            }
+                            catch (SocketException ex)
+                            {
+                                MessageBox.Show("Mất kết nối đến server: " + ex.Message);
+                            }
+
+                            string txt = System.Text.Encoding.UTF8.GetString(arr, 0, bytesReceived);
+                            text += txt;
+
+                        } while (text.Length > 0 && text[text.Length - 1] != '\n');
+
+                        if (!txt_Listen.IsDisposed)
+                        {
+                            txt_Listen.Invoke(new Action(() => txt_Listen.AppendText(text)));
+                        }
+                    }
+                    catch (SocketException ex)
+                    {
+                        MessageBox.Show("Lỗi kết nối đến server: " + ex.Message);
+                    }
+                    catch (ObjectDisposedException ex)
+                    {
+                        MessageBox.Show("Socket hoặc control đã bị dispose.");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Lỗi không xác định: " + ex.Message);
+                    }
+                }
+                txt_Listen.AppendText(Environment.NewLine);
+                listenerSocket.Close();
+            }
+
+            CheckForIllegalCrossThreadCalls = false;
+            Thread serverThread = new Thread(new ThreadStart(StartUnsafeThread));
+            serverThread.Start();
+        }
+    }
+}
